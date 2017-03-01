75624788a61cbdeb040679f5a99789c95509773a
Zelda Zhang <ze.zhang@hotmail.com>
2017-03-01 15:44:01 +0800

    start

---
+++ footer.php
@@ -0,0 +1,12 @@
+
+	<footer id="colophon" role="contentinfo">
+		<div class="container">
+        <p>&copy; 2014 <a href="<?php echo home_url(); ?>/"><?php bloginfo('name'); ?></a>.Theme By <a href="http://blog.ageme.info/">Amativeness</a>.由 <a href="http://http://wordpress.org" target="_blank">Wordpress</a> 强力驱动</p>
+    <p>设计风格参考<a href="http://www.dandyweng.com" target="_blank">Dandy Weng</a> 和 <a href="http://blog.sloger.info/" target="_blank">sloger's Blog</a></p>
+    </div>
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+<script src="<?php echo get_template_directory_uri(); ?>/js/script.js"></script>
+<?php wp_footer(); ?>
+</body>
+</html>
